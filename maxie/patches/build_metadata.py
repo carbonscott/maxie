f51f297135d0b957b7a12fe4066fd5b907e90d8c
carbonscott <wangimagine@gmail.com>
2024-05-19 23:34:07 -0400

    Add monkey patches. (Unfortunately, it does not solve the problem, but it is nevertheless a good direction.)

--- maxie/patches/build_metadata.py
+++ maxie/patches/build_metadata.py
@@ -3,7 +3,7 @@
 # - Discussion: https://github.com/pytorch/pytorch/pull/108915
 # - Diff: https://github.com/pytorch/pytorch/pull/108915/files
 # - Codes: https://github.com/b-chu/pytorch/blob/3ccc8f3357447f83ce56dd9d7618e520e4a11e97/torch/distributed/_shard/sharding_spec/chunk_sharding_spec.py
-import torch
+
 from dataclasses import dataclass
 import torch
 import torch.distributed._shard.sharded_tensor.metadata as sharded_tensor_meta
@@ -15,13 +15,15 @@ from torch.distributed._shard.sharded_tensor.utils import (
 from torch.distributed._shard._utils import narrow_tensor
 import torch.distributed as dist
 import torch.distributed.distributed_c10d as distributed_c10d
-from typing import List, Union, TYPE_CHECKING
+from typing import cast, List, Optional, Union, TYPE_CHECKING
 from torch.distributed._shard.sharding_spec._internals import (
     get_chunked_dim_size,
     get_split_size,
 )
 
 from torch.distributed._shard.sharding_spec.api import ShardingSpec
+
+from pkg_resources import packaging
 
 if TYPE_CHECKING:
     # Only include ShardedTensor when do type checking, exclude it
@@ -30,11 +32,13 @@ if TYPE_CHECKING:
 
 from torch.distributed._shard.sharding_spec.chunk_sharding_spec import ChunkShardingSpec
 
-
-def patched_build_metadata(self,
-                           tensor_sizes: torch.Size,
-                           tensor_properties: sharded_tensor_meta.TensorProperties,
-                           ) -> sharded_tensor_meta.ShardedTensorMetadata:
+# ----------------------------------------------------------------------- #
+#  Patch -- build_metadata
+# ----------------------------------------------------------------------- #
+def build_metadata(self,
+                   tensor_sizes: torch.Size,
+                   tensor_properties: sharded_tensor_meta.TensorProperties,
+                   ) -> sharded_tensor_meta.ShardedTensorMetadata:
     tensor_num_dim = len(tensor_sizes)
 
     self._verify_dim(self.dim)
@@ -66,5 +70,9 @@ def patched_build_metadata(self,
         tensor_properties
     )
 
-ChunkShardingSpec.build_metadata = patched_build_metadata
-print(f"ChunkShardingSpec.build_metadata has been patched...")
+def patch_build_metadata():
+    torch_version = torch.__version__
+    torch_version = torch_version[:torch_version.find("+") if "+" in torch_version else None]
+    if packaging.version.parse(torch_version) <= packaging.version.parse("2.0.1"):
+        ChunkShardingSpec.build_metadata = build_metadata
+        print(f"[RANK {dist.get_rank()}] ChunkShardingSpec.build_metadata has been patched...")
