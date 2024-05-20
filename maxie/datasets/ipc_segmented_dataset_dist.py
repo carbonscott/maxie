f9bb1cb78d87dc49a2a06ae7892a213b41f75150
b-crouch <isabella.crouch@berkeley.edu>
2024-05-20 11:15:11 -0700

    Randomize dataset entry generator

--- maxie/datasets/ipc_segmented_dataset_dist.py
+++ maxie/datasets/ipc_segmented_dataset_dist.py
@@ -3,6 +3,7 @@ import csv
 import json
 import socket
 import numpy as np
+import random
 
 from math import ceil
 from itertools import islice
@@ -90,6 +91,7 @@ class IPCDistributedSegmentedDataset(Dataset):
 
     def _init_entry_generator(self):
         PSANA_ACCESS_MODE = 'idx'
+        all_events = []
         for entry in self.json_entry_list:
             exp           = entry['exp'          ]
             run           = entry['run'          ]
@@ -98,8 +100,24 @@ class IPCDistributedSegmentedDataset(Dataset):
             num_events    = entry['num_events'   ]
             if events is None:
                 events = range(num_events)
-            for event in events:
-                yield (exp, run, PSANA_ACCESS_MODE, detector_name, event)
+            for event in entry['events'       ]:
+                all_events.append((exp, run, PSANA_ACCESS_MODE, detector_name, event))
+        # Randomize the order of events occurring across all runs of all experiments in dataset
+        random.shuffle(all_events)
+        for event_tuple in all_events:
+            yield event_tuple
+
+
+        # for entry in self.json_entry_list:
+        #     exp           = entry['exp'          ]
+        #     run           = entry['run'          ]
+        #     detector_name = entry['detector_name']
+        #     events        = entry['events'       ]
+        #     num_events    = entry['num_events'   ]
+        #     if events is None:
+        #         events = range(num_events)
+        #     for event in events:
+        #         yield (exp, run, PSANA_ACCESS_MODE, detector_name, event)
 
     def calculate_end_idx(self):
         # Calculate and return the end index for the current dataset segment.
