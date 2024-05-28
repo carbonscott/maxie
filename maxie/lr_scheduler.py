98f619441b3e5c35c8c4c272821e0d16560b4049
carbonscott <wangimagine@gmail.com>
2024-05-28 14:49:01 -0400

    Fix a bug in the scheduler: optimizer should be saved as an attribute.

--- maxie/lr_scheduler.py
+++ maxie/lr_scheduler.py
@@ -6,12 +6,13 @@ class CosineLRScheduler(_LRScheduler):
     """ Iteration can mean an epoch, a micro batch or a mini batch.
     """
     def __init__(self, optimizer, warmup_iterations, total_iterations, min_lr=0, last_iteration=-1):
+        self.optimizer         = optimizer
         self.warmup_iterations = warmup_iterations
         self.total_iterations  = total_iterations
         self.min_lr            = min_lr
         self.decay_iterations  = self.total_iterations - self.warmup_iterations
         self.last_iteration    = last_iteration
-        self.base_lrs          = [group['lr'] for group in optimizer.param_groups]
+        self.base_lrs          = [group['lr'] for group in self.optimizer.param_groups]
 
     def get_lr(self):
         """
