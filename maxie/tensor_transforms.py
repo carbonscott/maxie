efafd24ae8d95e2a162bbf38966fdcd5f58d2aa6
carbonscott <wangimagine@gmail.com>
2024-06-04 05:45:09 -0400

    Update batch perm sampler to support any dim.

--- maxie/tensor_transforms.py
+++ maxie/tensor_transforms.py
@@ -244,15 +244,17 @@ class Norm:
 
 
 class BatchPermSampler:
-    def __init__(self, sampling_fraction = None):
-        if sampling_fraction is not None and (sampling_fraction < 0.0 or sampling_fraction > 1.0):
+    def __init__(self, sampling_fraction = None, dim = 0):
+        if sampling_fraction is not None and (sampling_fraction <= 0.0 or sampling_fraction > 1.0):
             raise ValueError("sampling_fraction must be None or a number between 0 and 1.")
         self.sampling_fraction = sampling_fraction
+        self.dim               = dim
 
     def __call__(self, image_tensor, **kwargs):
         if self.sampling_fraction is not None:
-            B = image_tensor.size(0)
-            sample_size    = int(B * self.sampling_fraction)
-            sample_indices = torch.randperm(B)[:sample_size]
-            image_tensor   = image_tensor[sample_indices]
+            dim_size       = image_tensor.size(self.dim)
+            sample_size    = max(int(dim_size * self.sampling_fraction), 1)
+            sample_indices = torch.randperm(dim_size)[:sample_size]
+            image_tensor   = image_tensor.transpose(self.dim, 0)[sample_indices].transpose(0, self.dim)
+
         return image_tensor
