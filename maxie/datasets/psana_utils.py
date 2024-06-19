602bfdebeaa7ce8ff4ed11610824fa13a014eb2b
carbonscott <wangimagine@gmail.com>
2024-06-18 22:20:59 -0400

    Clean up.

--- maxie/datasets/psana_utils.py
+++ maxie/datasets/psana_utils.py
@@ -1,27 +1,6 @@
 import numpy as np
 import psana
 
-def apply_mask(data, mask, mask_value = np.nan):
-    """
-    Return masked data.
-
-    Args:
-        data: numpy.ndarray with the shape of (B, H, W).·
-              - B: batch of images.
-              - H: height of an image.
-              - W: width of an image.
-
-        mask: numpy.ndarray with the shape of (B, H, W).·
-
-    Returns:
-        data_masked: numpy.ndarray.
-    """
-    # Mask unwanted pixels with np.nan...
-    data_masked = np.where(mask, data, mask_value)
-
-    return data_masked
-
-
 class PsanaImg:
     """
     It serves as an image accessing layer based on the data management system
